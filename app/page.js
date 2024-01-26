0896f735348fac23b8df37c7b5e91d0667ebfad8
Dimitri <dfrancillette@gmail.com>
2024-01-26 20:11:26 +0100

    2 buttons added on the Home page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from './Home.module.css';
 import Header from "./components/Header";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,6 +12,15 @@ export default function Home() {
       </div>
       <div className="flex justify-center mt-6">
         <p className="font-semibold">Build you decks and craft your victory !</p>
+      </div>
+      <div className="flex justify-center my-10 space-x-5">
+        <Link href="/builder">
+          <button className="btn btn-secondary px-8">Build your Deck</button>
+        </Link>
+        <p className="mt-2 font-semibold">OR</p>
+        <Link href='/library'>
+          <button className="btn btn-success px-10">Find a Deck</button>
+        </Link>
       </div>
     </div>
   );

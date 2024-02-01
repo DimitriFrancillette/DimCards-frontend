ad1d48ad41022635ac3064d5989289dda826e946
Dimitri <dfrancillette@gmail.com>
2024-02-01 13:29:12 +0100

    the cards gallery page shows the cards images now

--- app/cards/page.js
+++ app/cards/page.js
@@ -1,12 +1,33 @@
+'use client'
 import React from 'react'
 import Header from "../components/Header";
+import { useState, useEffect } from 'react';
 
 const CardsPage = () => {
+  const [apiCards, setApiCards] = useState([]);
+
+  useEffect(() => {
+
+    fetch('http://localhost:3000/cards')
+      .then(response => response.json())
+      .then((data) => {
+        setApiCards(data.result);
+        // console.log(data.result)
+      });
+  }, []);
+
+  const cards = apiCards.map( data => {
+      return <img src={data.assets[0].gameAbsolutePath} alt={":x"} />
+  })
+
   return (
     <div>
-      <Header />      
-      CardsPage
+      <Header />
+      <div>
+      {cards}
       </div>
+      CardsPage
+    </div>
   )
 }
 

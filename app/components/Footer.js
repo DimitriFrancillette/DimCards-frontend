dc2a3bd5ee81bddc15af39631f5dbaac270a1a7d
Dimitri <dfrancillette@gmail.com>
2024-03-20 13:50:06 +0100

    modification in Header and Footer components

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import Link from 'next/link';
-import { GiAtomicSlashes } from "react-icons/gi";
-import { GiAnvilImpact } from "react-icons/gi";
-import { IoLibrarySharp } from "react-icons/io5";
-import { GiCardAceSpades } from "react-icons/gi";
-import { FaBoxArchive } from "react-icons/fa6";
-import { IoSettingsSharp } from "react-icons/io5";
 
-
-const Header = () => {
+const Footer = () => {
     return (
-        <div className="bg-sky-700 flex justify-center items-center min-h-16">
-            <p>© 2024 Dim's LOR Vault</p>
-        </div>
+        <footer>
+            <div className="bg-sky-700 flex justify-center items-center min-h-16">
+                <p>© 2024 Dim's LOR Vault</p>
+            </div>
+        </footer>
+
     )
 }
 
-export default Header
+export default Footer
--- app/components/Header.js
+++ app/components/Header.js
@@ -1,56 +1,54 @@
 import React from 'react'
 import Link from 'next/link';
-import { GiAtomicSlashes } from "react-icons/gi";
-import { GiAnvilImpact } from "react-icons/gi";
-import { IoLibrarySharp } from "react-icons/io5";
-import { GiCardAceSpades } from "react-icons/gi";
+import { GiAtomicSlashes, GiAnvilImpact, GiCardAceSpades } from "react-icons/gi";
+import { IoLibrarySharp, IoSettingsSharp } from "react-icons/io5";
 import { FaBoxArchive } from "react-icons/fa6";
-import { IoSettingsSharp } from "react-icons/io5";
-
 
 const Header = () => {
     return (
-        <div className="bg-sky-700 flex justify-between min-h-16">
-            <div className='flex min-w-80'>
-                <Link href="/" className='flex'>
-                    <GiAtomicSlashes className='size-14 mx-2' />
-                    <div className='text-2xl pt-4 font-bold'>
-                        Dim's LOR Vault
+        <header>
+            <div className="bg-sky-700 flex justify-between min-h-16">
+                <div className='flex min-w-80'>
+                    <Link href="/" className='flex'>
+                        <GiAtomicSlashes className='size-14 mx-2' />
+                        <div className='text-2xl pt-4 font-bold'>
+                            Dim's LOR Vault
+                        </div>
+                    </Link>
+                </div>
+                <div className='flex max-w-70'>
+                    <Link href="/cards">
+                        <div className='flex justify-around mt-3.5 min-w-40 mx-4'>
+                            <GiCardAceSpades className='size-8' />
+                            Cards Gallery
+                        </div>
+                    </Link>
+                    <Link href="/library">
+                        <div className='flex justify-around mt-3.5 min-w-40 mx-4'>
+                            <IoLibrarySharp className='size-8' />
+                            Deck Library
+                        </div>
+                    </Link>
+                    <Link href="/manager">
+                        <div className='flex justify-around mt-3.5 min-w-40 mx-4'>
+                            <FaBoxArchive className='size-8' />
+                            Deck Manager
+                        </div>
+                    </Link>
+                    <Link href="/builder">
+                        <div className='flex justify-around mt-3.5 min-w-40 mx-4'>
+                            <GiAnvilImpact className='size-8' />
+                            Deck Builder
+                        </div>
+                    </Link>
+                </div>
+                <div className='min-w-16'>
+                    <div className='mt-3.5'>
+                        <IoSettingsSharp className='size-8' />
                     </div>
-                </Link>
-            </div>
-            <div className='flex max-w-70'>
-                <Link href="/cards">
-                    <div className='flex justify-around mt-3.5 min-w-40 mx-4'>
-                        <GiCardAceSpades className='size-8' />
-                        Cards Gallery
-                    </div>
-                </Link>
-                <Link href="/library">
-                    <div className='flex justify-around mt-3.5 min-w-40 mx-4'>
-                        <IoLibrarySharp className='size-8' />
-                        Deck Library
-                    </div>
-                </Link>
-                <Link href="/manager">
-                    <div className='flex justify-around mt-3.5 min-w-40 mx-4'>
-                        <FaBoxArchive className='size-8' />
-                        Deck Manager
-                    </div>
-                </Link>
-                <Link href="/builder">
-                    <div className='flex justify-around mt-3.5 min-w-40 mx-4'>
-                        <GiAnvilImpact className='size-8' />
-                        Deck Builder
-                    </div>
-                </Link>
-            </div>
-            <div className='min-w-16'>
-                <div className='mt-3.5'>
-                    <IoSettingsSharp className='size-8' />
                 </div>
             </div>
-        </div>
+        </header>
     )
 }
 

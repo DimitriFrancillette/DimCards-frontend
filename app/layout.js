b24dc8fc2e1fb37460c3c9ff224ffc4b2197fec2
Dimitri <dfrancillette@gmail.com>
2024-03-21 14:14:32 +0100

    modified the 2 imports in the layout file

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
 
 const inter = Inter({ subsets: ["latin"] });
 

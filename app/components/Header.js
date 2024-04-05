df2ed123330780f252e871fd4f0865ee582da763
Dimitri <dfrancillette@gmail.com>
2024-04-05 21:54:57 +0200

    daisyUI buttons added in the Header

--- app/components/Header.js
+++ app/components/Header.js
@@ -9,42 +9,44 @@ const Header = () => {
         <header>
             <div className="bg-sky-700 flex justify-between min-h-14">
                 <div className='flex min-w-80'>
-                    <Link href="/" className='flex'>
-                        <GiAtomicSlashes className='size-14 mx-2' />
-                        <div className='text-2xl pt-4 font-bold'>
-                            Dim's LOR Vault
-                        </div>
+                    <Link href="/">
+                        <button className="btn no-animation btn-ghost h-full">
+                            <GiAtomicSlashes className='size-14 mx-2' />
+                            <div className='text-2xl font-bold'>
+                                Dim's LOR Vault
+                            </div>
+                        </button>
                     </Link>
                 </div>
                 <div className='flex max-w-70'>
                     <Link href="/cards">
-                        <div className='flex justify-around mt-3.5 min-w-40 mx-4'>
-                            <GiCardAceSpades className='size-8' />
+                        <button className="btn no-animation btn-ghost h-full flex justify-around items-center mx-4">
+                            <GiCardAceSpades className='size-10' />
                             Cards Gallery
-                        </div>
+                        </button>
                     </Link>
                     <Link href="/library">
-                        <div className='flex justify-around mt-3.5 min-w-40 mx-4'>
-                            <IoLibrarySharp className='size-8' />
+                        <button className="btn no-animation btn-ghost h-full flex justify-around items-center mx-4">
+                            <IoLibrarySharp className='size-10' />
                             Deck Library
-                        </div>
+                        </button>
                     </Link>
                     <Link href="/manager">
-                        <div className='flex justify-around mt-3.5 min-w-40 mx-4'>
-                            <FaBoxArchive className='size-8' />
-                            Deck Manager
-                        </div>
+                        <button className="btn no-animation btn-ghost h-full flex justify-around items-center mx-4">
+                            <FaBoxArchive className='size-10' />
+                            Deck Manager                        
+                        </button>
                     </Link>
                     <Link href="/builder">
-                        <div className='flex justify-around mt-3.5 min-w-40 mx-4'>
-                            <GiAnvilImpact className='size-8' />
-                            Deck Builder
-                        </div>
+                        <button className="btn no-animation btn-ghost h-full flex justify-around items-center mx-4">
+                            <GiAnvilImpact className='size-10' />
+                            Deck Builder                    
+                        </button>
                     </Link>
                 </div>
                 <div className='min-w-16'>
                     <div className='mt-3.5'>
-                        <IoSettingsSharp className='size-8' />
+                        <IoSettingsSharp className='size-10' />
                     </div>
                 </div>
             </div>
